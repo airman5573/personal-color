259bc27dacf616172df3679902864082242963fb
airman5573 <rpf5573@kakao.com>
2022-04-02 11:30:49 +0900

    :truck: chore: :construction: wip(test): sdf

--- src/Container.tsx
+++ src/Container.tsx
@@ -1,27 +1,36 @@
-import { useEffect, useState } from 'react';
+import { SizeMe } from 'react-sizeme';
 
 type Props = {
   children: JSX.Element;
 };
 
+const getMaxWidth = (width: number, height: number) => {
+  if (!width || !height) return;
+  const $app = document.querySelector('#root') as HTMLElement;
+  const $body = document.querySelector('body') as HTMLElement;
+  if (!$app || !$body) return;
+  const maxWidth = Math.floor(($body.clientHeight / height) * width);
+  return maxWidth;
+};
+
 function Container({ children }: Props) {
-  const [maxWidth, setMaxWidth] = useState<string>('100%');
-  useEffect(() => {
-    const $app = document.querySelector('#root') as HTMLElement;
-    const $body = document.querySelector('body') as HTMLElement;
-    if (!$app || !$body) return;
-    requestAnimationFrame(() => {
-      const _maxWidth = Math.floor(
-        ($body.clientHeight / $app.offsetHeight) * $app.clientWidth,
-      );
-      console.log('maxWidth : ', _maxWidth);
-      setMaxWidth(`${_maxWidth}px`);
-    });
-  }, []);
   return (
-    <div className="container pt-10 pb-10" style={{ maxWidth }}>
+    <div className="container pt-10 pb-10" style={{ maxWidth: '100%' }}>
       {children}
     </div>
+  );
+  return (
+    <SizeMe refreshRate={16} monitorHeight={true}>
+      {({ size }) => {
+        const { width, height } = size;
+        const maxWidth = getMaxWidth(width ?? 1, height ?? 1);
+        return (
+          <div className="container pt-10 pb-10" style={{ maxWidth: '100%' }}>
+            {children}
+          </div>
+        );
+      }}
+    </SizeMe>
   );
 }
 
